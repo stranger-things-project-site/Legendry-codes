6359a015db2054d3af43a4c1ccff241082b81ec5
Rohan Saini <83878702+rohansaini886@users.noreply.github.com>
2022-10-18 19:13:11 +0530

    Create Main.py

---
+++ Python/Main.py
@@ -0,0 +1,5 @@
+import random
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+for i in l:
+  print(str(i + random.choice(l)) + ' the value of i is ' + str(i) + 'then the value of ramdom is is also random like its name.!')
+    
